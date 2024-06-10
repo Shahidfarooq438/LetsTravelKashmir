7fe5626b41c8a1fc02551fb92b69821190a6154c
Shahidfarooq438 <shahidfarooq438@gmail.com>
2024-06-10 10:51:14 +0530

    Update MainCard.jsx

--- src/MainCard.jsx
+++ src/MainCard.jsx
@@ -2,10 +2,7 @@ import image from './assets/sonamarg.jpg'
 
 
 function MainCard(){
-
-    function handleClick(event){
-        
-    }
+    
     return (
         <div className="main-card" id='tour-packages'>
             <img src={image} alt="image" className='main-image'/>
@@ -14,7 +11,7 @@ function MainCard(){
             <p className="main-text">Would you explore natural paradise, Kashmir in the world, let’s find
                  the best destination in Kashmir with us.Explore all the destinations with us at affordable prices.
                  Travel with us once and you will feel th difference.</p>
-            <button className='main-button' onClick={handleClick}>Contact Us</button>
+            <a href='#contact-us'><button className='main-button'>Contact Us</button></a>
            </div>
         </div>
     );
